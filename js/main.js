f63e29062a08037774992bd4f32ed52f3160d36d
Joseph.Su <a607131731@gmail.com>
2017-04-25 03:58:35 +0800

    slide show

--- js/main.js
+++ js/main.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-function threeFunc(){
-
-    var arr = [];
-
-    for(var i = 0; i < 3; i++){
-        let j = i;
-        arr.push(function(){           
-            console.log(j);
-        });
-    }
-    return arr;
-}
-
-var fs = threeFunc();
-
-fs[0]();
-fs[1]();
-fs[2]();
-
-function threeFunc(){
-
-    var arr = [];
-
-    for(var i = 0; i < 3; i++){
-        arr.push(
-            (function(j){
-                return function(){
-                    console.log(j);
-                }
-            }(i))
-        );
-    }
-    return arr;
-}
-
-var fs1 = threeFunc();
-
-fs1[0]();
-fs1[1]();
-fs1[2]();
-
-
-=======
 var slideIndex = 1;
 var timeOut;
 showSlides(slideIndex);
@@ -75,4 +31,3 @@ function currentSlide(index){
     clearTimeout(timeOut);
     showSlides(slideIndex = index);
 }
->>>>>>> gh-pages
