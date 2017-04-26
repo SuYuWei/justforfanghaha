1c1ec394727eb33de11bb0d8dfda79591b23b97d
Joseph.Su <a607131731@gmail.com>
2017-04-27 02:18:27 +0800

    rwd nav bar

--- js/main.js
+++ js/main.js
@@ -8,6 +8,44 @@ menu.addEventListener("click",function(){
 	this.classList.toggle("open");
 });
 
+var triggers = document.querySelectorAll('.list');
+const bg  = document.querySelector('.dropdown-bg');
+const nav  = document.querySelector('.nav-menu');
+resize();
+window.addEventListener("resize", resize);
+
+function resize(){
+    var mq = window.matchMedia( "(max-width: 1000px)" );
+    console.log("eee");
+    if (mq.matches) {
+    // window width is at least 1000px
+        triggers.forEach(trigger => {
+            trigger.removeEventListener('mouseenter', handleEnter);
+            trigger.removeEventListener('mouseleave', handleLeave);
+        });
+    } else {
+    // window width is less than 1000px
+        triggers.forEach(trigger => {
+            trigger.addEventListener('mouseenter', handleEnter);
+            trigger.addEventListener('mouseleave', handleLeave);
+        });
+    }
+}
+
+
+var navBar = document.getElementById("nav-bar");
+var slideShow = document.getElementById("slide");
+window.addEventListener("scroll",function(e){
+    var windowScroll = this.scrollY;
+    var slideHeight = slideShow.offsetHeight
+    if(windowScroll > slideHeight) {
+        navBar.style.setProperty("background","black");
+    }else {
+        navBar.style.setProperty("background","transparent");
+    }
+})
+
+
 function showSlides(index){
     var slides = document.querySelectorAll(".mySlides");
     var dots = document.querySelectorAll(".dot");
@@ -27,7 +65,6 @@ function showSlides(index){
     },4000);
 }
 
-
 function plusSlides(index){
     clearTimeout(timeOut);
     showSlides(slideIndex += index);
@@ -36,5 +73,30 @@ function plusSlides(index){
 function currentSlide(index){
     clearTimeout(timeOut);
     showSlides(slideIndex = index);
+}
+
+function handleEnter(){
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+    bg.classList.add('open');    
+    const liWidth = this.getBoundingClientRect();
+    const dropdown = this.querySelector('.dropdown');
+	const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    const coords = {
+		width: liWidth.width,
+		height: dropdownCoords.height,
+        top: liWidth.top - navCoords.top,
+		left: liWidth.left - navCoords.left
+    };
+    dropdown.style.setProperty('width', `${coords.width}px`);
+    bg.style.setProperty('width', `${coords.width}px`);
+    bg.style.setProperty('height', `${coords.height}px`);
+    bg.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+}
+
+function handleLeave(){
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    bg.classList.remove('open');
 }
 
